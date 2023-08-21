0719a63d09bcca3834d4e7a2f83cb7e1b9257c6f
tresor <tshims79@gmail.com>
2023-08-21 14:58:54 +0200

    We undestand find, eq and contains functinality

--- CypressAutomation/cypress/integration/examples/Test1.js
+++ CypressAutomation/cypress/integration/examples/Test1.js
@@ -21,6 +21,7 @@ describe("Testing web", () => {
     cy.get(".product:visible").should("have.length", 4);
     // Parent child chaining
     cy.get(".products").find(".product").should("have.length", 4);
+    cy.get(".products").find(".product").eq(3).contains("ADD TO CART").click();
 
     //fixture
   });
