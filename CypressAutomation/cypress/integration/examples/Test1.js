2d441ff0a6387f905341a38941cf7517cbccac4c
tresor <tshims79@gmail.com>
2023-08-22 13:56:06 +0200

    I am happy with the foundation

--- CypressAutomation/cypress/integration/examples/Test1.js
+++ CypressAutomation/cypress/integration/examples/Test1.js
@@ -20,8 +20,32 @@ describe("Testing web", () => {
     cy.get(".product").should("have.length", 5);
     cy.get(".product:visible").should("have.length", 4);
     // Parent child chaining
-    cy.get(".products").find(".product").should("have.length", 4);
-    cy.get(".products").find(".product").eq(3).contains("ADD TO CART").click();
+    cy.get(".products").as('productLocator');
+    cy.get("@productLocator").find(".product").should("have.length", 4);
+    cy.get(":nth-child(3) > .product-action > button").click();
+    cy.get("@productLocator")
+      .find(".product")
+      .eq(3)
+      .contains("ADD TO CART")
+      .click().then(() => {
+        console.log('sf');
+      });
+
+    cy.get("@productLocator")
+      .find(".product")
+      .each(($el, index, $list) => {
+        const textVeg = $el.find("h4.product-name").text();
+        if (textVeg.includes("cashews")) {
+          cy.wrap($el).find("button").click();
+          //$el.find("button").click();
+        }
+      });
+
+      // Assert if logo text is present
+      cy.get(".brand").should("have.text", "GREENKART");
+      cy.get('.brand').then((logoElement) => {
+        cy.log(logoElement.text());
+      });
 
     //fixture
   });
