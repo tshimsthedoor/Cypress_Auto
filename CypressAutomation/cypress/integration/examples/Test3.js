fe251d1d6377587ceef2f693dc407426179d9f2c
tresor <tshims79@gmail.com>
2023-08-23 21:18:16 +0200

    I got it

---
+++ CypressAutomation/cypress/integration/examples/Test3.js
@@ -0,0 +1,10 @@
+/// <reference types="Cypress" />
+
+describe("My Third Test Suite", () => {
+  it("My first Test case", () => {
+    cy.visit("https://rahulshettyacademy.com/AutomationPractice/");
+   
+
+    //fixture
+  });
+});
